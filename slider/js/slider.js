94d0207a240f1e0bf97ab2fd4c4ae84b757140dc
zhangchunlish <zhangchunlish@yeah.net>
2018-06-29 18:05:30 +0800

    'ss'

--- slider/js/slider.js
+++ slider/js/slider.js
@@ -1,4 +1,3 @@
-
 function $(id){
     return document.getElementById(id);
 }
@@ -16,14 +15,58 @@ for(var i in imgs){
 }
 
 var spans=slider_ctrl.children;
+var iNow=0;
+var scrollWidth=500;
 for(var k in spans){
     spans[k].onclick=function(){
         if(this.className=='slider_ctrl_prev'){
-
+            animate(imgs[iNow],{left:scrollWidth});
+            --iNow<0?iNow=imgs.length-1:iNow;
+            imgs[iNow].style.left=-scrollWidth+'px';
+            animate(imgs[iNow],{left:0})
+            setSquare();
         }else if(this.className=='slider_ctrl_next'){
-
+            autoplay();
         }else{
-            
+            var that=this.innerHTML-1;
+            if(that>iNow){
+                animate(imgs[iNow],{left:-scrollWidth});
+                imgs[that].style.left=scrollWidth+'px';
+
+
+            }else if(that<iNow){
+                animate(imgs[iNow],{left:scrollWidth});
+                imgs[that].style.left=-scrollWidth+'px';
+            }
+            iNow=that;
+            animate(imgs[iNow],{left:0})
+            setSquare();
+
         }
     }
+}
+// 4、设置setSquare函数：
+function setSquare(){
+    for(var i =1;i<spans.length-1;i++){
+        spans[i].className='slider_ctrl_con';
+    }
+    spans[iNow+1].className='slider_ctrl_con current';
+}
+// 5、设置定时器
+var timer=null;
+timer=setInterval(autoplay,2000);
+function autoplay(){
+    animate(imgs[iNow],{left:-scrollWidth});
+    ++iNow>imgs.length-1?iNow=0:iNow;
+    imgs[iNow].style.left=scrollWidth+'px';
+    animate(imgs[iNow],{left:0})
+    setSquare();
+}
+// 6、鼠标经过的时候的效果：
+js_box.onmouseover=function(){
+    clearInterval(timer);
+}
+js_box.onmouseout=function(){
+    clearInterval(timer);
+    timer=setInterval(autoplay,2000);
 }
