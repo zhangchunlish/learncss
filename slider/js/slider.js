6e0cc7d328f6fbf103e38e4a0d4566abd4644e39
zhangchunlish <zhangchunlish@yeah.net>
2018-07-01 21:09:52 +0800

    ss

--- slider/js/slider.js
+++ slider/js/slider.js
@@ -1,29 +1,19 @@
-
 function $(id){
     return document.getElementById(id);
 }
-var js_box=$("js_box");
-var slider_main=$("slider_main_box");
-var imgs=slider_main.children;
-var slider_ctrl=$("slider_ctrl");
-
-for(var i in imgs){
-    var span=document.createElement("span");
-    span.className='slider_ctrl_con';
-    span.innerHTML=imgs.length-i;
-    slider_ctrl.insertBefore(span,slider_ctrl.children[1]);
-
-}
-
-var spans=slider_ctrl.children;
-for(var k in spans){
-    spans[k].onclick=function(){
-        if(this.className=='slider_ctrl_prev'){
-
-        }else if(this.className=='slider_ctrl_next'){
-
-        }else{
-            
-        }
+// 4、设置setSquare函数：
+function setSquare(){
+    for(var i =1;i<spans.length-1;i++){
+        spans[i].className='slider_ctrl_con';
     }
+    spans[iNow+1].className='slider_ctrl_con current';
 }
+// 5、设置定时器
+function autoplay(){
+    animate(imgs[iNow],{left:-scrollWidth});
+    ++iNow>imgs.length-1?iNow=0:iNow;
+    imgs[iNow].style.left=scrollWidth+'px';
+    animate(imgs[iNow],{left:0})
+    setSquare();
+}
+
